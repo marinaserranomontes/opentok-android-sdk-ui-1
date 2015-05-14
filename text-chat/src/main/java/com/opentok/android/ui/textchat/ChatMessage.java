aa1158db4bcee0ef0c889f413c439e67cdd874ae
marinas <marinaserranomontes@gmail.com>
2015-05-14 11:43:11 +0200

    Adding text-chat UI component

---
+++ text-chat/src/main/java/com/opentok/android/ui/textchat/ChatMessage.java
@@ -0,0 +1,75 @@
+package com.opentok.android.ui.textchat;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class ChatMessage {
+
+    public static enum MessageStatus {
+        SENT_MESSAGE,
+        RECEIVED_MESSAGE
+    }
+
+    protected String sender;
+    protected String text;
+    protected long timestamp;
+    protected MessageStatus status;
+    private UUID id;
+
+    public ChatMessage(String text) {
+        this.text = text;
+        this.id= UUID.randomUUID();
+    }
+
+    public ChatMessage(String sender, String text) {
+        this.sender = sender;
+        this.text = text;
+        this.id= UUID.randomUUID();
+    }
+
+    public ChatMessage(String sender, String text, MessageStatus status) {
+        this.sender = sender;
+        this.text = text;
+        this.id= UUID.randomUUID();
+        this.status = status;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public MessageStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MessageStatus status) {
+        this.status = status;
+    }
+
+    public void setTimestamp(long time) {
+        timestamp = time;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+}
